2cff8f6eecd7a92d27cea5c2740a5dfec04d9310
Gules <kkylejohnson@gmail.com>
2016-11-05 15:48:35 -0600

    started work

--- CsTextRPG/game.cs
+++ CsTextRPG/game.cs
@@ -4,9 +4,52 @@ namespace CsTextRPG
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		public static void Main(string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine("What is your name?");
+			string name = Console.ReadLine();
+			Character player = new Character();
+			player.name = name;
+			player.Rouge();
+			Console.WriteLine("Hello {0} the {1}", player.name, player.clas); 
+
+		}
+
+	}
+	class Character
+	{
+		public int lvl = 1;
+		public int sp = 10;
+		public string name;
+		public Array items;
+		public int exp=0;
+		public int gold=0;
+		public int limit=1;
+		public int hp;
+		public int chp;
+		public int m;
+		public int cm;
+		public int inte;
+		public int str;
+		public int def;
+		public int dex;
+		public int agi;
+		public string clas;
+
+		public void Rouge()
+		{
+			Console.WriteLine("rouge class");
+			int hp = 25;
+			int chp = 25;
+			int m = 15;
+			int cm = 15;
+			int inte = 10;
+			int str = 15;
+			int de = 10;
+			int dex = 20;
+			int agi = 25;
+			string clas = "Rouge";
+
 		}
 	}
 }
